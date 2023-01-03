6ee821689e643e04348a2031afe3cdcb201f7e6b
Sana-Ullah <Sk101tda@gmail.com>
2023-01-03 21:37:32 +0500

    Adding DataModeling.h File contains Structures of Task and Date.

--- DataModeling.h
+++ DataModeling.h
@@ -1,32 +1,38 @@
-#ifndef DATAMODELING_H
-#define DATAMODELING_H
-#define TITLE_SIZE 15
-#define DESCRIPTION_SIZE 30 
+#ifndef DATAMODELING_H // if not defined,
+#define DATAMODELING_H // define DATAMODELING_H
 
-enum TYPE
+/*-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+/*-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+// DEFINING THE CONSTANTS
+#define TITLE_SIZE 15       // Title Size
+#define DESCRIPTION_SIZE 30 // Description Size
+
+/*-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+/*-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+enum TYPE // Creating an enum TYPE, representing the following
 {
-    TODO,
-    DOING,
-    DONE,
-    EVENT
+    TODO,  // When a task is TODO
+    DOING, // When a task is DOING
+    DONE   // When a task is DONE
 };
 
-struct Date
+struct Date // Creating a struct Date for the following
 {
-    int day;
-    int month;
-    int year;
+    int day;   // the day
+    int month; // the month
+    int year;  // the year
 };
 
-struct Task
+struct Task // Creating a struct Task for the tasks that we will use
 {
-    int ID;
-    int priority;
-    char title[TITLE_SIZE];
-    char description[DESCRIPTION_SIZE];
-    struct Date *currentDate;
-    struct Date *dueDate;
-    enum TYPE type;
+    int ID;                             // the ID of the task
+    int priority;                       // the priority of the task
+    char title[TITLE_SIZE];             // the  title of the task
+    char description[DESCRIPTION_SIZE]; // the description of the task
+    struct Date *currentDate;           // the current date of the  task
+    struct Date *dueDate;               // the due date of the task
+    enum TYPE type;                     // the type of the task
 };
 
-#endif
+#endif // end of DATAMODELING_H
