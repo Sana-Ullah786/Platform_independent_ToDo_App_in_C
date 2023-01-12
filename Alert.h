61ffc78a3be88858f3ac968f071077d388e210ce
Sana-Ullah <Sk101tda@gmail.com>
2023-01-13 00:25:19 +0500

    Adding Alert.h file that contains the Function that launch a Python script and Generates and send a System Alert with Today's Due Tasks.

---
+++ Alert.h
@@ -0,0 +1,7 @@
+#ifndef ALERT_H // if not exists, then
+#define ALERT_H // define ALERT_H
+
+void checkTime(int *, char *); // Function to check time
+void sendAlert(char *);        // Function to send alert
+
+#endif // end of ALERT_H
